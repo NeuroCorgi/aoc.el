a7764158d8ae9a92bb69772b227aae8fe3682079
Aleksandr Pokatilov <pokatilov0802@gmail.com>
2023-11-30 13:36:05 +0100

    License and commentary added

--- aoc.el
+++ aoc.el
@@ -1,4 +1,31 @@
-;; File: aoc.el -*- lexical-binding: t; -*-
+;;; aoc.el --- Convenience functions for advent of code -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Aleksandr Pokatilov
+
+;; Version: 0.0.1
+;; URL: https://github.com/NeuroCorgi/aoc.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary
+
+;; This package provides convenience function to submit solutions
+;; for advent of code puzzles.
+;;
+;; Package caches all the downloaded inputs and submission to
+;; relieve load on the server.
+
 ;;; Code:
 
 (require 'url)
